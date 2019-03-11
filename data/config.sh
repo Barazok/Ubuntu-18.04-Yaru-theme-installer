f10e2b891db629c4b5fc91e7a340ae0b122e34ba
Lawrence Deneiple <barazok.blackrocks@gmail.com>
2019-03-11 12:23:56 +0100

    Rollback to Yaru 18.10.6

--- data/config.sh
+++ data/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MANAGER_VERSION="0.0.1"
-YARU_THEME_VERSION="19.04"
+YARU_THEME_VERSION="18.10.6"
 COUNTRY="fr"
 DOWNLOAD_PLATFORM="http://$COUNTRY.archive.ubuntu.com/ubuntu/pool/main/y/yaru-theme"
 DEFAULT_EDITOR=gedit
